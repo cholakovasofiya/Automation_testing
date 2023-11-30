9c9f29f5b3915eefb64fc0124820587f834a6515
cholakovasofiya <84577133+cholakovasofiya@users.noreply.github.com>
2023-11-30 09:35:34 +0200

    code changes

--- java_exercises/errors_and_exeptions/OutOfBoundsException.java
+++ java_exercises/errors_and_exeptions/OutOfBoundsException.java
@@ -5,7 +5,7 @@ public class OutOfBoundsException {
         try {
             String[] animals = {"cat", "dog", "mouse"};
             System.out.println(animals[3]);
-        } catch (Exception OutOfBoundsException) {
+        } catch (ArrayIndexOutOfBoundsException e) {
             assert System.out != null;
             System.out.println("Enter number between 0 and 2");
         }
